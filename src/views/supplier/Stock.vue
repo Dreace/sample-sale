a5f5819c45f69f873654336a437a0ec34afb530c
Dreace <Dreace@Foxmail.com>
2020-11-18 23:44:14 +0800

    增加库存查询

--- src/views/supplier/Stock.vue
+++ src/views/supplier/Stock.vue
@@ -1,8 +1,14 @@
 <template>
   <div>
-    <el-input v-model="input" v-on:input="reverse"></el-input>
-    <p>{{ reversed }}</p>
-    <el-table></el-table>
+    <el-button v-on:click="refreshSupplierStock">刷新</el-button>
+    <!--    <el-input v-model="input" v-on:input="reverse"></el-input>-->
+    <!--    <p>{{ reversed }}</p>-->
+    <el-table :data="supplierStocks">
+      <el-table-column prop="goodsId" label="商品编号"></el-table-column>
+      <el-table-column prop="goodsName" label="商品名称"></el-table-column>
+      <el-table-column prop="surplus" label="库存"></el-table-column>
+      <el-table-column prop="modifyTime" label="上次变动时间"></el-table-column>
+    </el-table>
   </div>
 </template>
 
@@ -10,15 +16,31 @@
 import api from "@/utils/api";
 import Vue from "vue";
 import Component from "vue-class-component";
+
+interface SupplierStockValue {
+  goodsId: number;
+  supplierId: number;
+  goodsName: string;
+  surplus: number;
+  modifyTime: number;
+}
+
 @Component
 export default class Stock extends Vue {
   input = "";
   reversed = "";
+  supplierStocks: SupplierStockValue[] = [];
 
   async reverse() {
     this.reversed = await api.post("test/reverse", {
       payload: this.input
     });
+  }
+
+  async refreshSupplierStock() {
+    this.supplierStocks = (await api.get(
+      "supplier/stocks"
+    )) as SupplierStockValue[];
   }
 }
 </script>
