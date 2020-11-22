f670b4d699a4826990887a28a0dfc8310146c2f7
Dai-Yi <897157752@qq.com>
2020-11-22 20:30:07 +0800

    注册功能完善2

--- src/views/login/Login.vue
+++ src/views/login/Login.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="login-container">
     <div class="login-title">我啪就点进来了,很快啊!</div>
-    <el-tabs class="tabs" stretch="true">
-      <el-tab-pane label="登陆">
+    <el-tabs v-model="activeTabName" class="tabs" :stretch="true">
+      <el-tab-pane name="LoginTab" label="登陆">
         <el-form
           :model="loginForm"
           :rules="loginRules"
@@ -33,54 +33,54 @@
           >
         </el-form>
       </el-tab-pane>
-      <el-tab-pane label="注册">
+      <el-tab-pane name="RegisterTab" label="注册">
         <el-form
-          :model="registeredForm"
-          :rules="registeredRules"
+          :model="registerForm"
+          :rules="registerRules"
           hide-required-asterisk
           class="el-form"
-          ref="registeredForm"
+          ref="registerForm"
         >
           <el-form-item prop="competence">
             <el-select
               class="selectRole"
-              v-model="registeredForm.competence"
+              v-model="registerForm.competence"
               placeholder="用户身份"
             >
-              <el-option label="客户" value="0"></el-option>
-              <el-option label="代理商" value="1"></el-option>
-              <el-option label="供货商" value="2"></el-option>
+              <el-option label="客户" :value="0"></el-option>
+              <el-option label="代理商" :value="1"></el-option>
+              <el-option label="供货商" :value="2"></el-option>
             </el-select>
           </el-form-item>
           <el-form-item prop="userName">
             <el-input
-              v-model="registeredForm.userName"
+              v-model="registerForm.userName"
               placeholder="用户名"
               prefix-icon="el-icon-user"
             />
           </el-form-item>
           <el-form-item prop="email">
             <el-input
-              v-model="registeredForm.email"
+              v-model="registerForm.email"
               placeholder="邮箱"
               prefix-icon="el-icon-message"
             />
           </el-form-item>
           <el-row :gutter="10">
-            <el-col span="12">
+            <el-col :span="12">
               <el-form-item prop="password">
                 <el-input
-                  v-model="registeredForm.password"
+                  v-model="registerForm.password"
                   placeholder="登陆密码"
                   show-password
                   prefix-icon="el-icon-lock"
                 />
               </el-form-item>
             </el-col>
-            <el-col span="12">
+            <el-col :span="12">
               <el-form-item prop="confirmPassword">
                 <el-input
-                  v-model="registeredForm.confirmPassword"
+                  v-model="registerForm.confirmPassword"
                   placeholder="确认登陆密码"
                   show-password
                   prefix-icon="el-icon-lock"
@@ -89,20 +89,20 @@
             </el-col>
           </el-row>
           <el-row :gutter="10">
-            <el-col span="12">
+            <el-col :span="12">
               <el-form-item prop="keyPassword">
                 <el-input
-                  v-model="registeredForm.keyPassword"
+                  v-model="registerForm.keyPassword"
                   placeholder="私钥密码"
                   show-password
                   prefix-icon="el-icon-key"
                 />
               </el-form-item>
             </el-col>
-            <el-col span="12">
+            <el-col :span="12">
               <el-form-item prop="confirmKeyPassword">
                 <el-input
-                  v-model="registeredForm.confirmKeyPassword"
+                  v-model="registerForm.confirmKeyPassword"
                   placeholder="确认私钥密码"
                   show-password
                   prefix-icon="el-icon-key"
@@ -111,9 +111,9 @@
             </el-col>
           </el-row>
           <el-button
-            class="registeredButton"
+            class="registerButton"
             type="primary"
-            @click="submitRegistered('registeredForm')"
+            @click="submitregister('registerForm')"
             >注册</el-button
           >
         </el-form>
@@ -136,7 +136,7 @@ interface LoginRulesCheck {
   userName: Array<object>;
   password: Array<object>;
 }
-interface RegisteredFormValue {
+interface RegisterFormValue {
   userName: string;
   competence: string;
   password: string;
@@ -145,7 +145,7 @@ interface RegisteredFormValue {
   confirmKeyPassword: string;
   email: string;
 }
-interface RegisteredRulesCheck {
+interface RegisterRulesCheck {
   userName: Array<object>;
   password: Array<object>;
   competence: Array<object>;
@@ -159,13 +159,14 @@ interface UserInformation {
   username: string;
   password: string;
   email: string;
-  competence: string;
+  competence: number;
   access_token: string;
 }
 @Component
 export default class Login extends Vue {
+  activeTabName = "LoginTab";
   loginForm: LoginFormValue = { userName: "", password: "" };
-  registeredForm: RegisteredFormValue = {
+  registerForm: RegisterFormValue = {
     userName: "",
     competence: "",
     password: "",
@@ -195,28 +196,28 @@ export default class Login extends Vue {
     ]
   };
   validatePassword = (
-    rule: RegisteredRulesCheck,
+    rule: RegisterRulesCheck,
     value: string,
     callback: (error: Error | void) => void
   ) => {
-    if (value !== this.registeredForm.password) {
+    if (value !== this.registerForm.password) {
       callback(new Error());
     } else {
       callback();
     }
   };
   validateKeyPassword = (
-    rule: RegisteredRulesCheck,
+    rule: RegisterRulesCheck,
     value: string,
     callback: (error: Error | void) => void
   ) => {
-    if (value !== this.registeredForm.keyPassword) {
+    if (value !== this.registerForm.keyPassword) {
       callback(new Error());
     } else {
       callback();
     }
   };
-  registeredRules: RegisteredRulesCheck = {
+  registerRules: RegisterRulesCheck = {
     competence: [
       { required: true, message: "请选择用户身份", trigger: "blur" }
     ],
@@ -295,14 +296,15 @@ export default class Login extends Vue {
         })) as UserInformation;
         if (res !== null) {
           //若登陆成功则跳转页面
+          console.log(res.competence);
           switch (res.competence) {
-            case "0": //供货商
+            case 0: //供货商
               await this.$router.push({ path: "stock" }); //stock页面
               break;
-            case "1": //代理商
+            case 1: //代理商
               //await this.$router.push({ path: "stock" }); //stock页面
               break;
-            case "2": //客户
+            case 2: //客户
               //await this.$router.push({ path: "stock" }); //stock页面
               break;
           }
@@ -314,17 +316,22 @@ export default class Login extends Vue {
       }
     });
   }
-  submitRegistered(formName: string) {
+  submitregister(formName: string) {
     const ref: ElForm = this.$refs[formName] as ElForm;
     ref.validate(async (valid: boolean) => {
       if (valid) {
-        const res = await api.post("user/registered", {
-          username: this.registeredForm.userName,
-          password: this.registeredForm.password
+        const res = await api.post("user/register", {
+          name: this.registerForm.userName,
+          password: this.registerForm.password,
+          email: this.registerForm.email,
+          competence: this.registerForm.competence,
+          keypassword: this.registerForm.keyPassword
         });
-        console.log("submit!");
-        // if (res !== null) {
-        // }
+        if (res !== null) {
+          this.activeTabName = "LoginTab"; //跳转回登陆页面
+          this.loginForm.userName = this.registerForm.userName;
+          this.loginForm.password = this.registerForm.password;
+        }
       } else {
         console.log("error submit!");
         return false;
@@ -346,7 +353,7 @@ export default class Login extends Vue {
   width: 100%;
   margin: 0 auto;
 }
-.registeredButton {
+.registerButton {
   width: 100%;
   margin: 0 auto;
 }
