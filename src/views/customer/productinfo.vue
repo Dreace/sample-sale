d63e79ee34d7352cd2128e1c69bb4655323b6600
Sakura925 <51183398+Sakura925@users.noreply.github.com>
2020-11-26 17:29:08 +0800

    添加购物车（完成）

--- src/views/customer/productinfo.vue
+++ src/views/customer/productinfo.vue
@@ -3,7 +3,21 @@
     <h1>商品信息</h1>
     <div style="margin-top: 15px;">
       <el-row :gutter="20">
-        <el-col :span="16">
+        <el-col :span="2.5">
+          <el-button
+            v-on:click="dialogFindCartVisible = true"
+            style="width: 120px;text-align: center"
+            >查看购物车</el-button
+          >
+        </el-col>
+        <el-col :span="2.5">
+          <el-button
+            v-on:click="refreshProduct"
+            style="width: 100px;text-align: center"
+            >刷 新</el-button
+          >
+        </el-col>
+        <el-col :span="18">
           <el-input
             placeholder="请输入内容"
             v-model="search"
@@ -25,12 +39,6 @@
             ></el-button>
           </el-input>
         </el-col>
-        <el-col :span="6">
-          <el-button v-on:click="refreshProduct">刷新</el-button>
-        </el-col>
-        <el-col :span="6">
-          <el-button v-on:click="FindCart">查看购物车</el-button>
-        </el-col>
       </el-row>
     </div>
 
@@ -47,11 +55,14 @@
         >>
         <el-table-column label="商品 ID" prop="stockId"></el-table-column>
         <el-table-column label="商品名称" prop="goodsName"></el-table-column>
+        <el-table-column label="库存" prop="surplus"></el-table-column>
         <el-table-column label="商品价格" prop="price"></el-table-column>
         <el-table-column label="操作">
           <template slot-scope="scope">
-            <el-button size="medium" @click="Add()">添加</el-button>
-            <el-button @click="FindInfo(scope.$index, tableData)"
+            <el-button type="primary" @click="AddCart(scope.$index, scope.row)"
+              >加入购物车</el-button
+            >
+            <el-button @click="FindInfo(scope.$index, scope.rows)"
               >查看详情</el-button
             >
           </template>
@@ -67,32 +78,32 @@
       >
         <span>
           <el-form
-            :model="form"
+            :model="stockInfoform"
             label-position="left"
             inline
             class="demo-table-expand"
           >
             <el-form-item label="商品编号：">
-              <span>{{ form.stockID }}</span>
+              <span>{{ stockInfoform.stockID }}</span>
             </el-form-item>
             <el-form-item label="商品名称：">
-              <span>{{ form.goodsName }}</span>
+              <span>{{ stockInfoform.goodsName }}</span>
             </el-form-item>
             <el-form-item label="生产日期：">
-              <span>{{ form.productDate }}</span>
+              <span>{{ stockInfoform.productDate }}</span>
             </el-form-item>
             <el-form-item label="库存：">
-              <span>{{ form.surplus }}</span>
+              <span>{{ stockInfoform.surplus }}</span>
             </el-form-item>
             <el-form-item label="供货商：">
-              <span>{{ form.supplier }}</span>
+              <span>{{ stockInfoform.supplier }}</span>
             </el-form-item>
             <el-form-item label="价格：">
-              <span>{{ form.price }}</span>
+              <span>{{ stockInfoform.price }}</span>
             </el-form-item>
             <el-row>
               <el-form-item label="描述：">
-                <span>{{ form.parameters }}</span>
+                <span>{{ stockInfoform.parameters }}</span>
               </el-form-item>
             </el-row>
           </el-form>
@@ -105,26 +116,85 @@
 
     <div class="ShopCart">
       <el-dialog
-        title="商品详情"
+        title="购物车"
         :visible.sync="dialogFindCartVisible"
         :close-on-click-modal="true"
       >
         <span>
-          <el-table :data="tableOrder"> </el-table>
+          <el-table :data="cartItems" show-summary :summary-method="summary">
+            <el-table-column prop="goodsName" label="名称"> </el-table-column>
+            <el-table-column prop="price" label="单价"></el-table-column>
+            <el-table-column prop="quantity" label="数量"></el-table-column>
+            <el-table-column prop="total" label="总价"></el-table-column>
+            <el-table-column label="操作">
+              <template slot-scope="scope">
+                <el-button
+                  type="primary"
+                  size="mini"
+                  @click="cartItems.splice(scope.$index, 1)"
+                  >移出</el-button
+                >
+              </template>
+            </el-table-column>
+          </el-table>
         </span>
         <span slot="footer" class="dialog-footer">
           <el-button @click="dialogFindCartVisible = false">取 消</el-button>
-          <el-button @click="Buy">下 单</el-button>
+          <el-button @click="Buy" :disabled="cartItems.length === 0"
+            >下 单</el-button
+          >
         </span>
       </el-dialog>
     </div>
+
+    <div class="AddToCart">
+      <el-dialog
+        title="确认商品信息"
+        :visible.sync="dialogAddVisible"
+        width="500px"
+        destroy-on-close
+      >
+        <el-form ref="addCartForm" :model="addCartForm" label-width="80px">
+          <el-form-item label="商品名称">
+            <el-input disabled v-model="addCartForm.goodsName"></el-input>
+          </el-form-item>
+          <el-form-item label="单价">
+            <el-input disabled v-model="addCartForm.price"></el-input>
+          </el-form-item>
+          <el-form-item label="购买数量">
+            <el-input-number
+              :min="1"
+              v-model="addCartForm.quantity"
+              v-on:change="
+                addCartForm.total = addCartForm.price * addCartForm.quantity
+              "
+            >
+            </el-input-number>
+          </el-form-item>
+          <el-form-item label="总价">
+            <el-input disabled v-model="addCartForm.total"></el-input>
+          </el-form-item>
+        </el-form>
+        <div slot="footer" class="dialog-footer">
+          <el-button
+            @click="
+              dialogAddVisible = false;
+              clearAddCartForm;
+            "
+            >取 消
+          </el-button>
+          <el-button type="primary" @click="AddCartConfirm">确 定</el-button>
+        </div>
+      </el-dialog>
+    </div>
+
     <div class="block" align="center">
       <el-pagination
         layout="prev, pager, next, sizes, total, jumper"
         :current-page.sync="currentPage1"
         :page-sizes="[5, 10, 15, 20]"
         :page-size="pagesizes"
-        :total="total"
+        :total="pagetotal"
         @current-change="handleCurrentChange"
         @size-change="handleSizeChange"
       >
@@ -137,34 +207,43 @@
 import api from "@/utils/api";
 import Vue from "vue";
 import Component from "vue-class-component";
+import { ElForm } from "element-ui/types/form";
 
 interface TableVale {
   stockId: number;
   goodsName: string;
-  price: string;
+  surplus: number;
+  price: number;
 }
-interface FormValue {
-  stockID: any;
-  goodsName: any;
-  productDate: any;
-  parameters: any;
-  surplus: any;
-  price: any;
-  supplier: any;
+interface AddCartValue {
+  stockId: number;
+  goodsName: string;
+  price: number;
+  total: number;
+  quantity: number;
 }
 
 @Component
 export default class Product extends Vue {
   tableData: TableVale[] = [];
+  currentStock: TableVale | null = null;
+  cartItems: AddCartValue[] = [];
   pagesizes = 10;
   currentPage1 = 1;
-  total = 1;
+  pagetotal = 1;
   search = "";
   searchSelect = "";
   dialogFindStockVisible = false;
   dialogFindCartVisible = false;
-
-  form = {
+  dialogAddVisible = false;
+  addCartForm: AddCartValue = {
+    price: 0,
+    quantity: 0,
+    total: 0,
+    stockId: -1,
+    goodsName: ""
+  };
+  stockInfoform = {
     stockID: "",
     goodsName: "",
     productDate: "",
@@ -175,38 +254,60 @@ export default class Product extends Vue {
     sign: ""
   };
 
-  FindInfo(index, rows) {
+  FindInfo(index: number, row: TableVale) {
     this.dialogFindStockVisible = true;
-    console.log(index);
-    console.log(rows.stockId);
   }
-  FindCart() {
-    this.dialogFindCartVisible = true;
-    alert("阿斯顿撒旦");
-  }
-  Buy() {
-    this.$confirm("是否确定下单?", "提示", {
-      confirmButtonText: "确定",
-      cancelButtonText: "取消",
-      type: "warning"
-    })
-      .then(() => {
-        this.$message({
-          type: "success",
-          message: "下单成功!"
-        });
-        console.log();
-      })
-      .catch(() => {
-        this.$message({
-          type: "info",
-          message: "已取消下单"
-        });
+  async Buy() {
+    const res = await api.post("customer/customerOrder", this.cartItems);
+    if (res) {
+      this.$message({
+        message: "下单成功",
+        type: "success"
       });
+    } else {
+      this.$message.error("下单失败，库存不足！");
+    }
+    this.dialogFindCartVisible = false;
   }
-  Add() {
-    alert("阿凡达是否");
+  AddCart(index: number, row: TableVale) {
+    this.dialogAddVisible = true;
+    this.currentStock = row;
+    this.addCartForm.quantity = 1;
+    this.addCartForm.goodsName = this.currentStock.goodsName;
+    this.addCartForm.stockId = this.currentStock.stockId;
+    this.addCartForm.price = this.currentStock.price;
+    this.addCartForm.total = this.currentStock.price;
   }
+  summary(): Array<string | number> {
+    const sums: Array<string | number> = ["合计", ""];
+    let quantity = 0,
+      total = 0;
+    for (const item of this.cartItems) {
+      quantity += item.quantity;
+      total += item.total;
+    }
+    sums.push(quantity, total);
+    return sums;
+  }
+  clearAddCartForm() {
+    (this.$refs.addCartForm as ElForm).resetFields();
+  }
+  AddCartConfirm() {
+    if (
+      this.currentStock &&
+      this.addCartForm.quantity > this.currentStock.surplus
+    ) {
+      this.$message({
+        message: `${this.currentStock.goodsName} 库存不足（剩余 ${this.currentStock.surplus} 件）`,
+        type: "error"
+      });
+      return;
+    }
+    this.cartItems.push(Object.assign({}, this.addCartForm));
+    this.clearAddCartForm();
+    this.dialogAddVisible = false;
+  }
+
   handleCurrentChange(page: number) {
     this.currentPage1 = page;
   }
@@ -218,8 +319,6 @@ export default class Product extends Vue {
   }
 
   async SearchStock() {
-    console.log(this.searchSelect);
-    console.log(this.search);
     this.tableData = (await api.post("customer/productinfo", {
       select: this.searchSelect,
       search: this.search
@@ -227,7 +326,7 @@ export default class Product extends Vue {
   }
   async refreshProduct() {
     this.tableData = (await api.get("customer/productinfo")) as TableVale[];
-    this.total = this.tableData.length;
+    this.pagetotal = this.tableData.length;
   }
 
   mounted() {
