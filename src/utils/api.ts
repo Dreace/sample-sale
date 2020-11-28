fd5427e05707c7d08b54510e1902eea478d4ce3a
linlhf <30626349+linlhf@users.noreply.github.com>
2020-11-28 16:41:56 +0800

    增加动作

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://sale-api.dreace.top/",
+  baseURL: "http://0.0.0.0",
   responseType: "json",
   withCredentials: true
 });
--- src/views/trace/trace.vue
+++ src/views/trace/trace.vue
@@ -4,57 +4,81 @@
       <el-form
         :label-position="labelPosition"
         label-width="80px"
-        :model="formLabelAlign"
+        :model="traceInfoForm"
         ><div id="title" class="title">
           <h1>商品追溯</h1>
         </div>
         <el-form-item label="商品ID">
           <el-row :gutter="16">
             <el-col :span="14">
-              <el-input v-model="formLabelAlign.name"></el-input>
+              <el-input v-model="goodsID"></el-input>
             </el-col>
             <el-col :span="2"
-              ><el-button type="primary" @click="onSubmit"
+              ><el-button type="primary" @click="Check()"
                 >查询</el-button
               ></el-col
             >
           </el-row>
         </el-form-item>
-        <el-form-item label="相信信息 :">
-          <span>fuck</span>
+        <el-form-item label="存储编号 :">
+          <span>{{ traceInfoForm.stockID }}</span>
         </el-form-item>
-        <el-form-item label="生成签名 :">
-          <span>fuck</span>
+        <el-form-item label="生产日期 :">
+          <span>{{ traceInfoForm.productionDate }}</span>
         </el-form-item>
-        <el-form-item label="交易结果 :">
-          <span>fuck</span>
+        <el-form-item label="价格 :">
+          <span>{{ traceInfoForm.price }}</span>
+        </el-form-item>
+        <el-form-item label="售出状态 :">
+          <span>{{ traceInfoForm.state }}</span>
+        </el-form-item>
+        <el-form-item label="校验签名 :">
+          <span>{{ signcheck }}</span>
         </el-form-item>
       </el-form>
     </div>
   </div>
 </template>
 
-<script>
-export default {
-  name: "trace",
+<script lang="ts">
+import api from "@/utils/api";
+import Vue from "vue";
+import Component from "vue-class-component";
 
-  methods: {
-    onSubmit() {
-      console.log("submit!");
+interface FormValue {
+  stockID: number;
+  productionDate: string;
+  price: number;
+  sign: string;
+  state: string;
+}
+@Component
+export default class Trace extends Vue {
+  formdata: FormValue[] = [];
+  labelPosition = "right";
+  goodsID = "";
+  signcheck = true;
+  traceInfoForm: FormValue = {
+    stockID: 0,
+    productionDate: "",
+    price: 0,
+    sign: "",
+    state: ""
+  };
+
+  async Check() {
+    const res = (await api.post("trace/Trace", {
+      goodsID: this.goodsID
+    })) as FormValue;
+    if (res !== null) {
+      this.traceInfoForm.stockID = res.stockID;
+      this.traceInfoForm.sign = res.sign;
+      this.traceInfoForm.state = res.state;
+      this.traceInfoForm.price = res.price;
+      this.traceInfoForm.productionDate = res.productionDate;
     }
-  },
-
-  data() {
-    return {
-      labelPosition: "left",
-      formLabelAlign: {
-        name: "",
-        region: "",
-        type: ""
-      }
-    };
   }
-};
+}
 </script>
 
 <style>
