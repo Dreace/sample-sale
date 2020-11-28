e0d02d6b0f8e75ef4b9ece7d10f1fb42295dedc7
Sakura925 <51183398+Sakura925@users.noreply.github.com>
2020-11-28 20:28:37 +0800

    杂项

--- src/views/customer/tradeInfo.vue
+++ src/views/customer/tradeInfo.vue
@@ -3,43 +3,41 @@
     <h1>订单管理</h1>
     <!-- 表格 -->
     <div id="trade">
-      <el-table
-        :data="
-          tableData.filter(
-            data =>
-              !search || data.name.toLowerCase().includes(search.toLowerCase())
-          )
-        "
-        style="width: 100%"
-        @selection-change="handleSelectionChange"
-      >
+      <el-table :data="tableData" style="width: 100%">
         <el-table-column label="订单编号" prop="orderID"></el-table-column>
         <el-table-column label="收款方" prop="agentID"></el-table-column>
-        <el-table-column label="订单时间" prop="orderDate"></el-table-column>
-        <el-table-column label="订单状态" prop="state"></el-table-column>
-        <el-table-column align="right">
-          <!-- eslint-disable-next-line -->
-          <template slot="header" slot-scope="scope">
-            <el-row :gutter="20">
-              <el-col align="right" :span="16">
-                <el-input
-                  v-model="search"
-                  size="medium"
-                  placeholder="输入关键字搜索"
-                  icon="el-icon-search"
-                ></el-input>
-              </el-col>
-              <el-col align="right" :span="4">
-                <el-button @click="RefreshOrder">重 置</el-button>
-              </el-col>
-            </el-row>
+        <el-table-column
+          label="订单时间"
+          prop="orderDate"
+          style="width: 400px"
+        ></el-table-column>
+        <!--        <template slot-scope="scope">-->
+        <!--          {{ new Date(scope.row.orderDate * 1000).toLocaleString() }}-->
+        <!--        </template>-->
+        <el-table-column label="订单状态" prop="state">
+          <template slot-scope="scope">
+            <el-tag
+              type="success"
+              v-if="scope.row.customerSign && scope.row.agentSign"
+              >已完成</el-tag
+            >
+            <el-tag v-else-if="scope.row.agentSign">已签名 </el-tag>
+            <el-tag type="warning" v-else>未签名 </el-tag>
           </template>
+        </el-table-column>
+        <el-table-column label="操作">
+          <!-- eslint-disable-next-line -->
           <!-- eslint-disable-next-line -->
           <template slot-scope="scope">
             <el-button size="mini" @click="Download(scope.$index, scope.row)"
               >下 载</el-button
             >
-            <el-button size="mini" @click="FindOrder(scope.$index, scope.row)"
+            <el-button
+              size="mini"
+              @click="
+                FindOrder(scope.$index, scope.row);
+                getOrderTable(scope.$index, scope.row);
+              "
               >查 看</el-button
             >
             <el-button
@@ -56,57 +54,105 @@
           class="order_dialog"
           :visible.sync="dialogOrderVisible"
           :show-close="true"
+          title="订单详情"
         >
-          <span>
-            <el-form
-              :model="OrderInfo"
-              label-position="left"
-              inline
-              class="demo-table-expand"
-            >
-              <el-form-item label="客户ID：">
-                <span>{{ OrderInfo.price }}</span>
-              </el-form-item>
-              <el-form-item label="收款方ID：">
-                <span>{{ OrderInfo.agentID }}</span>
-              </el-form-item>
-              <el-row>
-                <el-form-item label="代理签名：">
-                  <span>{{ OrderInfo.agentSign }}</span>
-                </el-form-item>
+          <el-form :model="OrderInfo" label-position="left" style="width: auto">
+            <el-form-item>
+              <el-row style="width: 100%">
+                <el-col style="width: 25%">
+                  <el-form-item label="客户ID：">
+                    <div>{{ OrderInfo.customerID }}</div>
+                  </el-form-item>
+                </el-col>
+                <el-col style="width: 25%">
+                  <el-form-item label="代理商ID：">
+                    <div>{{ OrderInfo.agentID }}</div>
+                  </el-form-item>
+                </el-col>
+                <el-col style="width: 50%">
+                  <el-form-item label="订单状态：">
+                    <el-tag type="success" v-if="OrderInfo.state === 2"
+                      >已完成</el-tag
+                    >
+                    <el-tag v-else-if="OrderInfo.state === 1">已签名</el-tag>
+                    <el-tag type="warning" v-else>未签名</el-tag>
+                  </el-form-item>
+                </el-col>
               </el-row>
-              <el-row>
-                <el-form-item label="客户签名：">
-                  <span>{{ OrderInfo.customerSign }}</span>
-                </el-form-item>
-              </el-row>
-              <el-row>
-                <el-form-item label="订单状态：">
-                  <span>{{ OrderInfo.state }}</span>
-                </el-form-item>
-              </el-row>
-              <el-form-item>
-                <template>
-                  <el-table :data="OrdertableData" style="width: 100%">
-                    <el-table-column prop="date" label="日期" width="180">
-                    </el-table-column>
-                    <el-table-column prop="name" label="姓名" width="180">
-                    </el-table-column>
-                    <el-table-column prop="address" label="地址">
-                    </el-table-column>
-                  </el-table>
-                </template>
+            </el-form-item>
+            <el-row>
+              <el-form-item label="代理签名：">
+                <span>{{ OrderInfo.agentSign }}</span>
               </el-form-item>
-            </el-form>
-          </span>
+            </el-row>
+            <el-row>
+              <el-form-item label="客户签名：">
+                <span>{{ OrderInfo.customerSign }}</span>
+              </el-form-item>
+            </el-row>
+          </el-form>
+          <template>
+            <el-table :data="OrdertableData" style="width: 99.9%">
+              <el-table-column prop="goods_id" label="货物编号">
+              </el-table-column>
+              <el-table-column prop="goodsName" label="商品名称">
+              </el-table-column>
+              <el-table-column prop="stockId" label="商品ID"> </el-table-column>
+              <el-table-column prop="production_date" label="生产日期">
+              </el-table-column>
+            </el-table>
+          </template>
           <span slot="footer" class="dialog-footer">
             <el-button type="primary" @click="dialogOrderVisible = false"
-              >确 定</el-button
+              >取 消</el-button
+            >
+            <el-button
+              type="primary"
+              @click="customerSignVisible = true"
+              :disabled="this.OrderInfo.state !== 1"
+              >签 名</el-button
             >
           </span>
         </el-dialog>
       </template>
     </div>
+    <div>
+      <el-dialog
+        title="客户签名"
+        :visible.sync="customerSignVisible"
+        destroy-on-close
+        append-to-body
+      >
+        <el-form :model="signForm" ref="signForm" label-width="80px">
+          <el-form-item label="私钥" required>
+            <input type="file" accept=".pem" @change="readPrivateKey($event)" />
+            <div v-if="privateKey">
+              {{ privateKey.getUserIds()[0] }}
+            </div>
+          </el-form-item>
+          <el-form-item
+            label="私钥密码"
+            prop="password"
+            :rules="[
+              { required: true, message: '私钥密码不能为空', trigger: 'blur' }
+            ]"
+          >
+            <el-input
+              type="password"
+              v-model="signForm.password"
+              clearable
+            ></el-input>
+          </el-form-item>
+        </el-form>
+        <div slot="footer" class="dialog-footer">
+          <el-button @click="customerSignVisible = false">取 消</el-button>
+          <el-button type="primary" @click="customerSignCheck"
+            >确认签名</el-button
+          >
+        </div>
+      </el-dialog>
+    </div>
+
     <div class="block" align="center">
       <el-pagination
         layout="prev, pager, next, sizes, total, jumper"
@@ -122,54 +168,44 @@
   </div>
 </template>
 
-<style>
-.demo-table-expand {
-  font-size: 0;
-}
-
-.demo-table-expand label {
-  width: 90px;
-  color: #99a9bf;
-}
-
-.demo-table-expand .el-form-item {
-  margin-right: 0;
-  margin-bottom: 0;
-  width: 50%;
-}
-
-.order_dialog {
-  width: 100%;
-  height: 100%;
-}
-</style>
+<style></style>
 
 <script lang="ts">
-import { Component, Vue } from "vue-property-decorator";
-import api from "@/utils/api"; // @ is an alias to /src
+import Component from "vue-class-component";
+import { ElForm } from "element-ui/types/form";
+import Vue from "vue";
+import api from "@/utils/api";
+import { key } from "openpgp";
+import Key = key.Key; // @ is an alias to /src
+// import { verify, cleartext, signature, key, sign } from "openpgp";
 
 interface TableValue {
   orderID: number;
   agentID: number;
-  orderDate: string;
+  orderDate: number;
   state: number;
 }
-
+interface SignFormValue {
+  password: string;
+}
 interface OrderInfoValue {
   customerID: number;
   agentID: number;
-  orderDate: string;
+  orderDate: number;
   state: number;
   agentSign: string;
   customerSign: string;
   orderID: number;
 }
 interface OrdertableDataValue {
-  goodsID: number;
-  stockID: number;
-  productionDate: string;
+  goods_id: number;
+  stockId: number;
+  production_date: number;
   price: number;
-  goodsSign: string;
+  sign: string;
+  state: number;
+  goodsName: string;
+  parameters: string;
 }
 
 @Component
@@ -177,21 +213,38 @@ export default class Order extends Vue {
   tableData: TableValue[] = [];
   search = "";
   dialogOrderVisible = false;
+  customerSignVisible = false;
   currentPage1 = 1;
   pagesizes = 10;
   pagetotal = 1;
   OrderInfo: OrderInfoValue = {
     customerID: 0,
     agentID: 0,
-    orderDate: "",
+    orderDate: 0,
     state: 0,
     agentSign: "",
     customerSign: "",
     orderID: 0
   };
   OrdertableData: OrdertableDataValue[] = [];
+  signForm: SignFormValue = {
+    password: ""
+  };
+  privateKey: Key.Key | null = null;
+  readPrivateKey(event: Event) {
+    const fileReader = new FileReader();
+    fileReader.onload = async () => {
+      this.privateKey = (await key.readArmored(fileReader.result)).keys[0];
+    };
+    const files = (event.target as HTMLInputElement).files;
+    if (files) {
+      fileReader.readAsText(files[0]);
+    }
+  }
 
-  Download(index: number, row: OrderInfoValue) {
+  async Download(index: number, row: TableValue) {
+    console.log(row);
+
     alert("download");
   }
 
@@ -203,32 +256,44 @@ export default class Order extends Vue {
     this.pagesizes = size;
   }
 
-  handleSelectionChange(val: number) {
-    console.log(val);
-  }
-
   async FindOrder(index: number, row: TableValue) {
     this.dialogOrderVisible = true;
-    alert("我趴就点进去了");
-    console.log(row);
-    // this.OrderInfo = (await api.post("customer/OrderInfo", row)) as OrderInfoValue;
+    const res = (await api.post("customer/OrderInfo", row)) as OrderInfoValue;
+    if (res !== null) {
+      this.OrderInfo.customerID = res.customerID;
+      this.OrderInfo.customerSign = res.customerSign;
+      this.OrderInfo.agentID = res.agentID;
+      this.OrderInfo.agentSign = res.agentSign;
+      this.OrderInfo.orderID = res.orderID;
+      this.OrderInfo.orderDate = res.orderDate;
+      this.OrderInfo.state = res.state;
+    }
   }
-
+  async getOrderTable(index: number, row: TableValue) {
+    this.OrdertableData = (await api.post(
+      "customer/OrderInfoTable",
+      row
+    )) as OrdertableDataValue[];
+  }
   async Delete(index: number, row: TableValue) {
     const res = await api.post("customer/OrderDelete", row);
     if (res !== null) {
-      console.log(res);
       this.$message({
         type: "success",
         message: res + "删除成功!"
       });
     }
   }
+  customerSignCheck() {
+    alert("签名失败");
+  }
 
   async RefreshOrder() {
     this.tableData = (await api.get("customer/Orders")) as TableValue[];
+    this.pagetotal = this.tableData.length;
     this.search = "";
   }
+
   mounted() {
     this.RefreshOrder();
   }
